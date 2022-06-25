be672d8273b5be2995807bcf2bd982124526cfa3
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-06-25 13:45:53 +0100

    Retrofit Api setup

---
+++ app/src/main/java/com/example/newsapp/utlils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utlils
+
+class Constants {
+    companion object{
+        const val API_KEY = "62e578ab8a2d44ababde06aa12228854"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
