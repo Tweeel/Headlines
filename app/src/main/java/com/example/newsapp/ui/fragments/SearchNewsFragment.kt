bfb6e6acd91340d4e58a0b858d1c15d70c05a2f8
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2022-06-30 16:50:24 +0100

    searching News

--- app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -2,17 +2,79 @@ package com.example.newsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.adapter.NewsAdapter
 import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.utlils.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.example.newsapp.utlils.Resource
 import com.example.newsapp.viewModel.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment: Fragment(R.layout.fragment_search_news)  {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        etSearch.addTextChangedListener {
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                it?.let {
+                    if(it.toString().isNotEmpty()){
+                        viewModel.searchNews(it.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        print(message)
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
--- app/src/main/java/com/example/newsapp/utlils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utlils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "62e578ab8a2d44ababde06aa12228854"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
